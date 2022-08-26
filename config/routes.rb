878fb437923c3be43cfa375f9b2f5881105875ec
NiNinanana <yeen00123@gmail.com>
2022-08-26 09:58:19 +0900

    더하기 계산기 작성

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
 
   # 주소로 처음 들어가면 HomeController에서 index액션에 연결해주세요
   root "home#index" # get "/" => "home#index" 와 동일
-  
   get "/hi" => "home#hi"
+
+  get "/calc" => "calc#index"
+  get "/calc/result" => "calc#result"
+
+  get "/calc/plus/:num1/:num2" => "calc#plus"
 end
